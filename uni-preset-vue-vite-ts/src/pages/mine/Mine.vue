25decb75d0ec6b23dbb3ab195cb3474ca8614fee
胡方旖 <1439600905@qq.com>
2024-11-06 20:46:55 +0800

    第二次提交

--- uni-preset-vue-vite-ts/src/pages/mine/Mine.vue
+++ uni-preset-vue-vite-ts/src/pages/mine/Mine.vue
@@ -3,9 +3,35 @@
 </script>
 
 <template>
-
+  <view class="header">
+    <view class="head">
+      <uni-icons type="bars" color="#fff" size="30"></uni-icons>
+      <text>登录</text>
+      <uni-icons type="contact" size="30"></uni-icons>
+    </view>
+   
+  </view>
+  <view>
+    <uni-icons type="contact" size="30"></uni-icons>
+  </view>
 </template>
 
 <style lang="scss" scoped>
-
+.header{
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+  width: 100vw;
+  .head{
+    height: 100rpx;
+    background-color: #c20c0c;
+    display: flex;
+    align-items: center;
+    color: #fff;
+    font-size: 18px;
+    font-weight: 700;
+    justify-content: space-between;
+    padding: 0 40rpx;
+  }
+}
 </style>
