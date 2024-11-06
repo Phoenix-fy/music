810db72512c5cc9db697a147ad5f0f1dfd2f0507
胡方旖 <1439600905@qq.com>
2024-11-06 22:59:41 +0800

    第三次

--- uni-preset-vue-vite-ts/src/pages/mine/Mine.vue
+++ uni-preset-vue-vite-ts/src/pages/mine/Mine.vue
@@ -3,46 +3,72 @@
 </script>
 
 <template>
-  <view class="header">
-    <view class="head">
-      <uni-icons type="bars" color="#fff" size="30"></uni-icons>
-      <text>登录</text>
-      <uni-icons type="search" size="30" color="#fff"></uni-icons>
-    </view>
-  
     <view>
-      <uni-icons type="person" size="50" color="#000"></uni-icons>
-      <view class="form">
-        <view class="row"><input type="text" placeholder="请输入手机号"></view>
-        <view class="row captcha">
-          <input type="text" placeholder="请输入验证码" v-model="captcha">
-          <button type="primary">1233</button>
-        </view>
-        <view class="row"><button type="primary">登录</button></view>
-      </view>
+      <navigator url="/pages/login/login">
+        <button>去登录</button>
+      </navigator>
     </view>
-  </view>
-  <view>
-    <uni-icons type="contact" size="30"></uni-icons>
-  </view>
+    <!-- <template>
+      <view class="header">
+        <image class="avatar" ></image>
+        <view class="nickname">
+        </view>
+        <view class="desc">
+          <view class="desc-tag">
+            <text class="bold"></text>关注
+          </view>
+          <view class="desc-tag">
+            <text class="bold"></text>粉丝
+          </view>
+          <view class="desc-tag">
+            <text class="bold"></text>等级
+          </view>
+          <view class="desc-tag">
+            <text class="bold">首</text>听歌
+          </view>
+        </view>
+      </view>
+      <uni-list>
+        <uni-list-item
+        ></uni-list-item>
+      </uni-list>
+    </template> -->
 </template>
 
 <style lang="scss" scoped>
-.header{
-  margin: 0;
-  padding: 0;
-  height: 100vh;
-  width: 100vw;
-  .head{
-    height: 100rpx;
-    background-color: #c20c0c;
-    display: flex;
-    align-items: center;
-    color: #fff;
-    font-size: 18px;
-    font-weight: 700;
-    justify-content: space-between;
-    padding: 0 40rpx;
+.header {
+    height: 450rpx;
+    overflow: hidden;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
   }
-}
+  .avatar {
+    width: 150rpx;
+    height: 150rpx;
+    border-radius: 50%;
+    border: 1px solid #ffffff;
+    margin: 100rpx auto 20rpx;
+    display: block;
+  }
+  .nickname {
+    text-align: center;
+    color: #ffffff;
+    font-weight: bold;
+  }
+  .desc {
+    display: flex;
+    justify-content: center;
+    color: #cfc4c4;
+    font-size: 14px;
+    padding: 20rpx 0;
+  }
+  .desc-tag {
+    margin: 0 20rpx;
+  }
+  .bold {
+    font-weight: bold;
+    margin-right: 10rpx;
+    color: #ffffff;
+  }
 </style>
