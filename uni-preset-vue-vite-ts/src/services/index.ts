a9dccd4f139f2b0604ad93ec91c3043d52706e25
左雪妙 <1478865647@qq.com>
2024-11-07 15:47:50 +0800

    发现第一次提交

--- uni-preset-vue-vite-ts/src/services/index.ts
+++ uni-preset-vue-vite-ts/src/services/index.ts
@@ -9,6 +9,24 @@ interface BannerRes {
   banners: BannerItem[];
   code: number;
 }
+export interface playItem {
+  coverImgUrl: string;
+  name: string;
+  id:number
+}
+
+interface playRes{
+  playlists:playItem[],
+  code:number
+}
 export const getBannerApi = () => {
   return request<BannerRes>({url:'https://zyxcl.xyz/music/api/banner'})
+}
+
+export const topPlaylistApi = () => {
+  return request<playRes>({ url: 'https://zyxcl.xyz/music/api/top/playlist',
+    data:{
+      limit:10
+      // order:new
+}})
 }
