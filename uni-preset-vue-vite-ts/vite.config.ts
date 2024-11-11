b0d673ae1958c21c94745b6bc234f8a674c7c502
胡方旖 <1439600905@qq.com>
2024-11-11 14:42:25 +0800

    最后提交

--- uni-preset-vue-vite-ts/vite.config.ts
+++ uni-preset-vue-vite-ts/vite.config.ts
@@ -1,7 +1,38 @@
-import { defineConfig } from "vite";
-import uni from "@dcloudio/vite-plugin-uni";
+import { defineConfig } from "vite"
+import uni from "@dcloudio/vite-plugin-uni"
+import path from 'path';
+const INVALID_CHARS_REGEX = /[\u0000-\u001F"#$&*+,:;<=>?[\]^`{|}\u007F]/g
+const DRIVE_LETTER_REGEX = /^[a-zA-Z]:/i
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // resolve: {
+  //   alias: {
+  //     '@': path.resolve(__dirname, 'src'),
+  //     '@dcloudio': path.resolve(__dirname, 'node_modules/@dcloudio')
+  //   }
+  // },
   plugins: [uni()],
-});
+  base: 'music',
+  server: {
+    watch: {
+      usePolling: true,  // 启用轮询机制，确保文件更改能被检测到
+      interval: 100,     // 设置轮询间隔为 100ms
+    },
+    hmr: true,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        sanitizeFileName(name) {
+          const match = DRIVE_LETTER_REGEX.exec(name)
+          const driveLetter = match ? match[0] : ''
+          return (
+            driveLetter +
+            name.slice(driveLetter.length).replace(INVALID_CHARS_REGEX, '')
+          )
+        }
+      }
+    }
+  }
+})
