d2fe65954e244ff7ff6b616f9d28cca40f6dc98b
胡方旖 <1439600905@qq.com>
2024-11-11 14:22:49 +0800

    Updates

---
+++ assets/pages-index-components-handlist-Myradar.DxNC7VbE.js
@@ -0,0 +1,1 @@
+import{a as s,b as a,m as n,o,M as t}from"./index-DA2Sepye.js";import{_ as r}from"./uni-icons.U29EC_Be.js";const e={__name:"Myradar",setup(e){const i=()=>{t()};return(t,e)=>{const c=s(a("uni-icons"),r);return o(),n(c,{type:"left",size:"30",onClick:i})}}};export{e as default};
